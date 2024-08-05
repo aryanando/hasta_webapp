d9dcda57a0c87bcefaa250b80aaab33e63ac7e06
Aryanando PC Kantor <aryanando232@gmail.com>
2024-08-05 13:48:45 +0700

    Add Select Slip data

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,8 +41,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e6611028bd1c29b50b06671ef4e13ed
 "icons/Icon-512.png": "d7c0083ce4a283a06180898c85e95d96",
 "icons/Icon-maskable-192.png": "e277f8ccf01ea2a4a3fecf1228839a96",
 "icons/Icon-maskable-512.png": "d7c0083ce4a283a06180898c85e95d96",
-"index.html": "94e474a7d531a62af7fc5a9263c25f6e",
-"/": "94e474a7d531a62af7fc5a9263c25f6e",
+"index.html": "84578441ab418d0a223fb40aeba360f6",
+"/": "84578441ab418d0a223fb40aeba360f6",
 "main.dart.js": "1c5d1ac45f9d932830c50821439c27f5",
 "manifest.json": "8cdf2532036c9b14b2dc1f60264bfb4a",
 "version.json": "73e0b5a67a72e2f4fd74f61ded32334d"};
