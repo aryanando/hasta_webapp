0cb463053618a483485642ff10482d9c8211c2ae
Aryanando PC Kantor <aryanando232@gmail.com>
2025-03-05 18:54:34 +0700

    Fixing Gaji

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2855885103"
+    serviceWorkerVersion: "2449740099"
   }
 });
